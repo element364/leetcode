380b9782545117fc6e930d4c29c8dc0543629960
element364 <element@idev.by>
2024-05-27 11:23:24 +0200

    Solved 189 Rotate Array

--- 0189-rotate-array/main.go
+++ 0189-rotate-array/main.go
@@ -3,27 +3,26 @@ package main
 import "fmt"
 
 func rotate(nums []int, k int) {
-	k = k % len(nums)
-	for i := 0; i < k; i++ {
-		prev := nums[0]
-		for j := 1; j < len(nums); j++ {
-			tmp := nums[j]
-			nums[j] = prev
-			prev = tmp
-		}
-		nums[0] = prev
+	n := len(nums)
+	k = k % n
+
+	result := make([]int, n)
+	for j := 0; j < n; j++ {
+		nj := (j + k) % n
+		result[nj] = nums[j]
 	}
+	copy(nums, result)
 }
 
 func main() {
 	fmt.Println("Example 1")
-	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	nums := []int{1, 2, 3, 4, 5, 6, 7} // [5,6,7,1,2,3,4]
 	k := 3
 	rotate(nums, k)
 	fmt.Println(nums)
 
 	fmt.Println("Example 2")
-	nums = []int{-1, -100, 3, 99}
+	nums = []int{-1, -100, 3, 99} // [3,99,-1,-100]
 	k = 2
 	rotate(nums, k)
 	fmt.Println(nums)
